0b0e5ebde50c94e4c31dddd57c0e4c884b240d67
João Gabriel <main.joaogabrielsg@gmail.com>
2025-04-23 11:49:37 -0300

    feat(posts): add infinite scroll in posts list

--- src/modules/app/modules/posts/hooks/queries/use-list-posts-query.ts
+++ src/modules/app/modules/posts/hooks/queries/use-list-posts-query.ts
@@ -1,11 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+/* eslint-disable perfectionist/sort-objects */
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { postsQueryKey } from "../../config/query-key";
 import { postsService } from "../../services/posts.service";
 
 export function useListPostsQuery() {
-  return useQuery({
-    queryFn: () => postsService.list(),
+  return useInfiniteQuery({
     queryKey: postsQueryKey.posts.lists(),
+    queryFn: ({ pageParam }) =>
+      postsService.list({ limit: pageParam.limit, offset: pageParam.offset }),
+    initialPageParam: {
+      limit: 5,
+      offset: 0,
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const url = new URL(lastPage.next);
+        const parameters = Object.fromEntries(url.searchParams.entries());
+        return {
+          limit: Number(parameters.limit),
+          offset: Number(parameters.offset),
+        };
+      }
+
+      return;
+    },
   });
 }
